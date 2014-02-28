8a45f00865112f70a07259a1cc06858602d55d09
Jelle Spekken <jspekken@gmail.com>
2014-02-28 11:22:50 +0100

    master layout jquery update

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -74,8 +74,8 @@
 			base_url: '{{ route("homepage") }}'
 		}
 	</script>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="{{ asset('javascripts/jquery-1.10.2.min.js') }}"><\/script>')</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="{{ asset('javascripts/jquery-1.11.0.min.js') }}"><\/script>')</script>
 	<script src="{{ asset('javascripts/vendor.min.js') }}"></script>
 	<script src="{{ asset('javascripts/application.min.js') }}"></script>
 
